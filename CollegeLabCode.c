5fe55d2b1078b15ef82fe6bfd65fa1be22e01b9e
Ashish Shah <112050986+thisisashishshah@users.noreply.github.com>
2023-02-07 22:44:46 +0530

    Update CollegeLabCode.c

--- CollegeLabCode.c
+++ CollegeLabCode.c
@@ -109,3 +109,35 @@ int main()
 	
 	}
 }
+
+
+
+// bubble sort
+#include<stdio.h>
+int main()
+{
+	int a[100],i,j,temp=0,n;
+	printf("Enter the number of elements:\n");
+	scanf("%d",&n);
+	printf("Enter the values:\n");
+	for(i=0;i<n;i++)
+	   scanf("%d",&a[i]);
+	printf("Original values are:\n");
+ 	for(i=0;i<n;i++)
+ 	    printf("%d\t",a[i]);
+ 	for(i=1;i<n;i++)
+ 	   {
+ 	   	for(j=0;j<n-i;j++)
+ 	   	   {
+ 	   	   	if(a[j]>a[j+1])
+ 	   	   	  {
+ 	   	   	  	temp=a[j];
+ 	   	   	  	a[j]=a[j+1];
+ 	   	   	  	a[j+1]=temp;
+					 }
+			   }
+		}
+	printf("\nThe sorted elements are:\n");
+	for(i=0;i<n;i++)
+	   printf("%d\t",a[i]);
+} 
